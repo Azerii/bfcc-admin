8350e814dbd95f694aeb1c85c2b61d2aa2c79ac2
Odinaka Ezennia <ogidi58@gmail.com>
2021-08-19 21:22:18 +0100

    complete form components up to dropdowns

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import FormGroup from "./components/FormGroup";
 import Layout from "./components/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Layout></Layout>
+      <Layout>
+        <FormGroup
+          inputType="text"
+          fieldStyle="shortText"
+          name="test"
+          placeholder="test"
+        />
+        <FormGroup
+          inputType="text"
+          fieldStyle="search"
+          name="test_search"
+          placeholder="test"
+          list={["item 1", "item2", "item 3", "item 4", "item 5"]}
+        />
+        <FormGroup
+          inputType="text"
+          fieldStyle="multiline"
+          name="test_multiline"
+          placeholder="test"
+        />
+        <FormGroup
+          inputType="text"
+          fieldStyle="dropdown"
+          name="test_dropdown"
+          placeholder="test"
+          list={["item 1", "item2", "item 3", "item 4", "item 5"]}
+        />
+      </Layout>
     </div>
   );
 }
